e41ae75edd7791dd84deb8cbf8174d9cdee8a3a2
Eren Özmen <erenzmenn@gmail.com>
2023-01-14 18:56:02 +0300

    Animatons & Animator
    Player run animation added , jumpforce increased 5 to 12, gravity changed 1 to 3

--- 2D Platformer Game/Assets/PlayerControl.cs
+++ 2D Platformer Game/Assets/PlayerControl.cs
@@ -5,26 +5,51 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D rb;
-    
+    private Animator anm;
+    private float dX = 0f;
+    private SpriteRenderer sprite;
+    [SerializeField]private float moveSpeed = 7f;
+    [SerializeField]private float jumpForce = 12f;
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anm = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dX * 7f, rb.velocity.y);
+        dX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dX * moveSpeed, rb.velocity.y);
 
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 5f);     
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);     
         }
-    
+        UpdateAnimationUpdate();
+        
+    }
+
+    private void UpdateAnimationUpdate()
+    {
+        if (dX > 0f)
+        {
+            anm.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dX < 0f)
+        {
+            anm.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anm.SetBool("running", false);
+        }
     }
 }
  
